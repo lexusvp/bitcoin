adb10c086d957b4dd2a55b9804e5e11765737f19
abstract⁷ <lexusvp001@gmail.com>
2024-12-01 03:31:32 +0800

    Create BITCOIN_MASTERSv1.js

---
+++ BITCOIN_MASTERSv1.js
@@ -0,0 +1,16 @@
+gpg: /home/codespace/.gnupg/trustdb.gpg: trustdb created
+gpg: key 099BAD163C70FBFA: public key "Will Clark <will8clark@gmail.com>" imported
+gpg: key 0A41BDC3F4FAFF1C: public key "Aaron Clauson (sipsorcery) <aaron@sipsorcery.com>" imported
+gpg: key 74810B012346C9A6: public key "Wladimir J. van der Laan <laanwj@protonmail.com>" imported
+gpg: key 796C4109063D4EAF: public key "Jon Atack <jon@atack.com>" imported
+gpg: key 410108112E7EA81F: public key "Hennadii Stepanov (hebasto) <hebasto@gmail.com>" imported
+gpg: key 8E4256593F177720: public key "Oliver Gugger <gugger@gmail.com>" imported
+gpg: key 944D35F9AC3DB76A: public key "Michael Ford (bitcoin-otc) <fanquake@gmail.com>" imported
+gpg: key 2EBB056FD847F8A7: public key "Stephan Oeste (it) <it@oeste.de>" imported
+gpg: key C37B1C1D44C786EE: public key "Duncan Dean <duncangleeddean@gmail.com>" imported
+gpg: key E13FC145CD3F4304: public key "Antoine Poinsot <darosior@protonmail.com>" imported
+gpg: key D7CC770B81FD22A8: public key "Ben Carman <benthecarman@live.com>" imported
+gpg: key 17565732E08E5E41: public key "Ava Chow <me@achow101.com>" imported
+gpg: key 188CBB2648416AD5: public key ".0xB10C <b10c@b10c.me>" imported
+gpg: Total number processed: 13
+gpg:               imported: 13
